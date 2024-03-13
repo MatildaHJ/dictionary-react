fc3010b01ad9f8b4a4a62edb11362d7726511ba8
MatildaHJ <matilda.hj.larsson@gmail.com>
2024-03-14 00:03:10 +0100

    footer with link to github repo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Dictionary />
+        <footer>
+          This is an{" "}
+          <a
+            href="https://github.com/MatildaHJ/dictionary-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open source code
+          </a>{" "}
+          by <strong>Matilda Larsson</strong>
+        </footer>
       </div>
     </div>
   );
