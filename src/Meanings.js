893d5c92efef0701078cf48704a8ad65135fb813
MatildaHJ <matilda.hj.larsson@gmail.com>
2024-03-13 23:53:04 +0100

    some search results css

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Meanings.css";
 
 export default function Meanings(props) {
   console.log(props.meanings);
   return (
-    <div className="Meaning">
+    <div className="Meanings">
       <h4>{props.meanings.partOfSpeech}</h4>
       <p>{props.meanings.definition}</p>
       <em>
