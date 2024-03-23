5116182854ca3c93d23ec4b43bb0fbe42bfa986a
MatildaHJ <matilda.hj.larsson@gmail.com>
2024-03-23 14:08:51 +0100

    synonyms component

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 import "./Meanings.css";
 
 export default function Meanings(props) {
@@ -6,10 +7,14 @@ export default function Meanings(props) {
   return (
     <div className="Meanings">
       <h4>{props.meanings.partOfSpeech}</h4>
+      <h5>
+        <strong>Definition: </strong>
+      </h5>
       <p>{props.meanings.definition}</p>
       <em>
         <p className="word-example">{props.meanings.example}</p>
       </em>
+      <Synonyms synonyms={props.meanings.synonyms} />
     </div>
   );
 }
---
+++ src/Synonyms.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./Synonyms.css";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms">
+        {props.synonyms.map((synonym, index) => (
+          <span className="synonym-words" key={index}>
+            {synonym}{" "}
+          </span>
+        ))}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
